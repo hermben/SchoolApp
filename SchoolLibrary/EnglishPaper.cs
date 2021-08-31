eafe3d6b86288683a3c34c516690bc5c22ea9825
Herman Benao <hermben@gmail.com>
2021-08-31 13:25:00 -0700

    adds ExtensionMethods.cs

---
+++ SchoolLibrary/EnglishPaper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public class EnglishPaper
+    {
+            public string Title { get; set; }
+            public int MinimumWordCount { get; set; }
+            public string paperText { get; set; }
+            public int WordCount
+            {
+                get { return paperText.WordCount(); }
+
+            }
+        
+    }
+}
