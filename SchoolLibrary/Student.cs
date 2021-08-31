3332f456644a610e394879d7795a27a9b8c4ea21
Herman Benao <hermben@gmail.com>
2021-08-31 12:28:07 -0700

    adds student.cs

---
+++ SchoolLibrary/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public class Student : Person
+    {
+        public enum Gradelevels { Freshman, Sophomore, Junior, Senior }
+        public Gradelevels Gradelevel { get; set; }
+
+        public override float ComputeGradeAverage()
+        {
+            return 4.0f;
+        }
+
+        public override string Sendmessage(string message)
+        {
+            var original = base.Sendmessage(message);
+            var sb = new StringBuilder(original);
+            sb.AppendLine("This is a private and confidential.");
+            return sb.ToString();
+        }
+    }
+}
